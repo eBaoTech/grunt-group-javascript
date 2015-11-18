986df3bdf246cec699e5fb43818e526c02b6bd66
brad.wu <brad.wu@ebaotech.com>
2015-11-18 22:19:55 +0800

    first version done

---
+++ README.md
@@ -0,0 +1,87 @@
+# grunt-group-javascript
+
+> Search and package javascript files in HTML
+
+## Getting Started
+This plugin requires Grunt `~0.4.5`
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-group-javascript --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-group-javascript');
+```
+
+## The "group_javascript" task
+
+### Overview
+This task will concat the javascript files between `startLine` and `endLine`, and replace the javascript lines in html file with `<script src='bundle-hash-date.js'></script>`. All javascript files will be concatenated into one file at the same folder with target html file.
+In your project's Gruntfile, add a section named `group_javascript` to the data object passed into `grunt.initConfig()`.
+
+```js
+grunt.initConfig({
+  group_javascript: {
+    options: {
+      sourcePathPrefix: 'test'
+    },
+    files: {
+      'test/expected': ['test/fixtures/*.html']
+    }
+  },
+});
+```
+
+### Options
+
+#### options.startLine
+Type: `String`  
+Default value: `'<!-- Project javascripts starts here -->'`  
+
+To flag the javascript references start, must be a single line.  
+
+#### options.endLine
+Type: `String`  
+Default value: `'<!-- Project javascripts ends here -->'`
+
+To flag the javascript references end, must be a single line.  
+
+#### options.sourcePathPrefix
+Type: `String`  
+
+The source path prefix of source file.  
+Eg. when `sourcePathPrefix: 'test'`, source file `'target': 'test/fixtures/index.html'` should be copy to `target/fixtures/index.html`  
+
+#### options.algorithm
+Type: `String`  
+Default value: `md5`  
+
+Algorithm of crypto, refers to [crypto](https://www.npmjs.com/package/crypto)  
+
+#### options.hashLength
+Type: `String`  
+Default value: 8  
+
+Hash length of crypto, refers to [crypto](https://www.npmjs.com/package/crypto)  
+
+#### options.date
+Type: `Boolean`  
+Default value: `true`  
+
+Add today to file name of `bundle.js`  
+
+#### options.date
+Type: `Boolean`  
+Default value: `true`  
+
+Create `bundle.js` as `bundle.min.js`. Not really minify javascript, only change the file name.
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## License
+MIT
---
+++ tasks/group_javascript.js
@@ -0,0 +1,143 @@
+/*
+ * grunt-group-javascript
+ * https://github/com/ebaotech/grunt-group-javascript
+ *
+ * Copyright (c) 2015 brad.wu
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var crypto = require('crypto');
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('group_javascript', 'Search and package javascript files in HTML', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      // punctuation: '.',
+      // separator: ', ',
+      startLine: '<!-- Project javascripts starts here -->',
+      endLine: '<!-- Project javascripts ends here -->',
+      algorithm: 'md5',
+      hashLength: 8,
+      min: true,
+      date: true
+    });
+
+    if (!options.sourcePathPrefix) {
+        grunt.fail.fatal('"sourcePathPrefix" can not be empty.');
+    }
+
+    var today = new Date();
+    var month = today.getMonth() + 1;
+    month = month >= 10 ? month : ('0' + month);
+    var day = today.getDate();
+    day = day >= 10 ? day : ('0' + day);
+    today = today.getFullYear() + '' + month + day;
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      var lineRegExp = /\r\n|\r|\n/;
+      var jsTagSrcRegExp = /(['|"]{1})(.*\.jsx?)(['|"]{1})/;
+
+      // Concat specified files.
+      var src = f.src.filter(function(filePath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filePath)) {
+          grunt.log.warn('Source file "' + filePath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filePath) {
+        // Read file source.
+        grunt.log.writeln('Process file [' + filePath + '] starting...');
+        var fileContent = grunt.file.read(filePath);
+        // grunt.log.writeln(fileContent.length);
+        var lines = fileContent.split(lineRegExp);
+        // grunt.log.writeln(lines ? lines.length : 'No line read.');
+
+        var needPackageJavascriptFiles = false;
+        var scriptTags = [];
+        var newLines = [];
+        var scriptTagIndex = -1;
+        // scan html file to find the project javascript files
+        // push to script files
+        lines.forEach(function(line, index) {
+            if (line.indexOf(options.startLine) !== -1) {
+                newLines.push(line);
+                needPackageJavascriptFiles = true;
+                scriptTagIndex = index + 1;
+            } else if (line.indexOf(options.endLine) !== -1) {
+                newLines.push(line);
+                needPackageJavascriptFiles = false;
+            } else if (needPackageJavascriptFiles) {
+                scriptTags.push(line);
+            } else {
+                newLines.push(line);
+            }
+        });
+        // grunt.log.writeln(scriptTagIndex);
+        if (scriptTags.length !== 0) {
+            var fileFolder = '';
+            var lastFileSeparatorIndex = filePath.lastIndexOf('/');
+            if (lastFileSeparatorIndex !== -1) {
+                fileFolder = filePath.substring(0, lastFileSeparatorIndex + 1);
+            }
+            var scriptTagsPath = [];
+            var scriptsContent = scriptTags.map(function(tag) {
+                var match = jsTagSrcRegExp.exec(tag);
+                if (match != null) {
+                    var scriptFilePath = fileFolder + match[2];
+                    if (!grunt.file.exists(scriptFilePath)) {
+                        grunt.log.error('Javascript file "' + scriptFilePath + '" not found.');
+                    } else {
+                        return '// from file:\r// ' + scriptFilePath + '\r' + grunt.file.read(scriptFilePath);
+                    }
+                }
+                return null;
+            }).filter(function(script) {
+                return script != null;
+            }).join('\r\r');
+
+            var targetFilePath = filePath.substring(options.sourcePathPrefix.length);
+            if (targetFilePath[0] === '/') {
+                targetFilePath = targetFilePath.substring(1);
+            }
+            var targetFolder = f.dest;
+            if (targetFolder[targetFolder.length - 1] !== '/') {
+                targetFolder = targetFolder + '/';
+            }
+            targetFilePath = targetFolder + targetFilePath;
+            // grunt.log.writeln(targetFilePath);
+            var hash = crypto.createHash(options.algorithm).update(scriptsContent).digest('hex');
+            var suffix = hash.slice(0, options.hashLength);
+            var scriptFileName = 'bundle-' + suffix +  (options.date ? ('-' + today) : '') + (options.min ? '.min' : '') + '.js';
+            var targetScriptFilePath = targetFilePath.substring(0, targetFilePath.lastIndexOf('/')) + '/' + scriptFileName;
+            grunt.file.write(targetScriptFilePath, scriptsContent);
+            grunt.log.writeln('Target javascript file "' + targetScriptFilePath + '" created.');
+
+            newLines.splice(scriptTagIndex, 0, '<script src="' + scriptFileName + '"></script>');
+            grunt.file.write(targetFilePath, newLines.join('\r'));
+            grunt.log.writeln('File [' + filePath + '] processed.');
+        }
+
+        return newLines.join('\r');
+      });
+      //.join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      // src += options.punctuation;
+
+      // Write the destination file.
+      // grunt.file.write(f.dest, src);
+
+      // Print a success message.
+      // grunt.log.writeln('File "' + f.dest + '" created.');
+    });
+  });
+};
